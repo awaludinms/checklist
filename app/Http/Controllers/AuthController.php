dc7b1238fc4487d23314576a4f7e1b1b3eb293f2
Awaludin <awaludinr830@gmail.com>
2020-11-19 05:07:20 +0700

    init

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        //
+
+    }
+
+    //
+    public function register(Request $request)
+    {
+
+    }
+}
